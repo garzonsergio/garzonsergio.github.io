f635e451f81867cde406caff1c411bde22986548
scgarzonp <scgarzonp@gmail.com>
2024-08-05 13:40:52 -0500

    Fix:Hero has been updated in vite

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,22 +3,27 @@ import styles from "./Hero.module.css"
 export function Hero() {
     return (
         <section className={styles.hero}>
-            <h1 className="hero-title">
-                App Developer & Mechanical Engineer
+            <h1 className={styles.heroTitle}>
+                {`Sergio Camilo Garzón`}
             </h1>
-            <h3 className="hero-text">
-                website in construction by Sergio Camilo Garzón
+            <h3 className={styles.heroText}>
+                Frontend Developer & Mechanical Engineer
             </h3>
-            <div className="bulb-track">
-                <div className="bulb top"></div>
-                <div className="bulb screw1"></div>
-                <div className="bulb screw2"></div>
-                <div className="bulb screw3"></div>
+            <div className={styles.bulbTrack}>
+                <div className={`${styles.bulb} ${styles.top}`}></div>
+                <div className={`${styles.bulb} ${styles.screw1}`}></div>
+                <div className={`${styles.bulb} ${styles.screw2}`}></div>
+                <div className={`${styles.bulb} ${styles.screw3}`}></div>
 
             </div>
-            <div className="container-phone">
-                <div className="cellphone"></div>
-                <div className="notch"></div>
+            <div></div>
+            <div className={styles.containerPhone}>
+                <div className={styles.screen}>
+                    
+                </div>
+                <div className={styles.notch}></div>
+                <div className={styles.keyboard}></div>
+                <div className={styles.pad}></div>
             </div>
 
         </section>
