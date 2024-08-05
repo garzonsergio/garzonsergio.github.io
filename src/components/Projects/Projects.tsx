c8a3b41969ebbd5ac505b02830b27b2e516b42cf
scgarzonp <scgarzonp@gmail.com>
2024-08-05 17:31:52 -0500

    Fix:An idea of how to show projects was created

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,9 +1,19 @@
 import styles from "./Projects.module.css"
 
-export  function Projects() {
-  return (
-    <section className={styles.projects} id="myprojects">
+export function Projects() {
+    return (
+        <section className={styles.projects} id="myprojects">
+            <div className={styles.container}>
+                <div className={styles.navigator}>
+                    <div className={styles.close}></div>
+                    <div className={styles.minimize}></div>
+                    <div className={styles.maximize}></div>
+                </div>
+                <h1 className={styles.projectsTitle}>{`Geoportal`}</h1><h3 className={styles.projectsText}>Sistema de Alerta Temprana</h3>
+                <div className={styles.picture}>
 
-    </section>
-  )
+                </div>
+            </div>
+        </section>
+    )
 }
